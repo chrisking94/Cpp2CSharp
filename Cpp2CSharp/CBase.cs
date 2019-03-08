96820d8189a1e88a7b36803071db50ef4450434e
chris <1050243371@qq.com>
2019-03-08 10:17:26 +0800

    set

---
+++ Cpp2CSharp/CBase.cs
@@ -0,0 +1,44 @@
+﻿/* ==============================================================================
+ * author		：chris
+ * create time	：3/7/2019 4:30:07 PM
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using CommonDll;
+using static CommonDll.Base;
+
+namespace Cpp2CSharp
+{
+    public class CBase
+    {
+        #region data
+        #region property
+        #endregion
+
+        #region field
+        #endregion
+        #endregion
+
+        #region constructor
+        protected CBase() { }
+        #endregion
+
+        #region interface
+        public static void JumpWhite(string s, ref int index)
+        {
+            while (s[index] == '\r' ||
+                s[index] == '\n' ||
+                s[index] == '\t' ||
+                s[index] == ' ') ++index;
+        }
+        #endregion
+
+        #region private
+        #endregion
+    }
+}
---
+++ Cpp2CSharp/CMacro.cs
@@ -0,0 +1,47 @@
+﻿/* ==============================================================================
+ * author		：chris
+ * create time	：3/7/2019 4:26:43 PM
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using CommonDll;
+using static CommonDll.Base;
+
+namespace Cpp2CSharp
+{
+    public class CMacro : CBase
+    {
+        #region data
+        #region property
+        #endregion
+
+        #region field
+        #endregion
+        #endregion
+
+        #region constructor
+        protected CMacro() { }
+        #endregion
+
+        #region interface
+        public static bool Parse(string s, ref int index)
+        {
+            JumpWhite(s, ref index);
+            if(s[index] == '#')
+            {
+                ++index;
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region private
+        #endregion
+    }
+}
